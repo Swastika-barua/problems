e48513b59e6caef4af4266385bc1a326b3057536
Swastika-barua <62758158+Swastika-barua@users.noreply.github.com>
2020-03-29 20:05:41 +0530

    while loop
    printing odd and even numbers from 1 to 50 using while loop

---
+++ evennumber.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int n=1;
+	while(n<=50)
+	{
+	 if(n%2==0)
+	 {
+	 
+	 	printf("even: %d\n",n);
+	 	
+	 	n++;
+}
+	
+	 	else if(n%2==1)
+		 {
+		 
+		 
+	 	printf("odd: %d",n);
+	 	n++;
+	 }
+	 	
+	 }
+	 	
+	 return 0;
+}
+	
+
