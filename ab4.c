066a83cfd1a62da9670a3cde14a7b13903aae6b6
Swastika-barua <62758158+Swastika-barua@users.noreply.github.com>
2020-04-03 21:08:47 +0530

    3.04.20

---
+++ ab4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int const X=0;
+	switch(5/4/3)
+	{
+		case 'X' :printf("Clinton");
+		break;
+		case 'X+1' :printf("Gandhi");
+		break;
+		case 'X+2' :printf("Gates");
+		break;
+		default :printf("brown");
+		break;
+	}
+}
